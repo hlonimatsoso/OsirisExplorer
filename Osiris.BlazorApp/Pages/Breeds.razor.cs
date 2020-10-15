1d3d08be93bca7c1370296d0edc4cec9e2d3b269
hloni2010 <hloni2010@gmail.com>
2020-10-15 23:24:28 +0200

    Added other pages

---
+++ Osiris.BlazorApp/Pages/Breeds.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Osiris.DogApi;
+using Osiris.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Osiris.BlazorApp.Pages
+{
+    public class BreedsBase : ComponentBase
+    {
+        [Inject] public IDogsApiClient DogsClient { get; set; }
+
+        [Inject] private ILogger<BreedsBase> Logger { get; set; }
+
+
+        public ApiResult<List<Breed>> AllBreedsResult { get; set; }
+
+        protected async override void OnInitialized()
+        {
+            AllBreedsResult = await DogsClient.GetAllBreeds();
+            await InvokeAsync(() => { StateHasChanged(); });
+
+        }
+    }
+}
