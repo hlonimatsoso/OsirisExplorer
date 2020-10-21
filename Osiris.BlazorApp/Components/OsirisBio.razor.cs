079c542f4f0534bc60cc3de77576d491e29209e4
hloni2010 <hloni2010@gmail.com>
2020-10-21 10:15:27 +0200

    Bio started

---
+++ Osiris.BlazorApp/Components/OsirisBio.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Osiris.BlazorApp.Components
+{
+    public class OsirisBioBase:ComponentBase
+    {
+       [Parameter] public string Name { get; set; }
+    }
+}
---
+++ Osiris/Bio.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Osiris
+{
+    public class Bio : ComponentBase
+    {
+        [Parameter] public string Name { get; set; }
+    }
+}
